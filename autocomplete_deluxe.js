90d95d3acefc830a1d78abc5e12bf1cc7b8ce7dc
Sebastian <sep.gil@gmail.com>
2012-09-11 11:57:52 +0200

    Added autogrow plugin for the multiple widget.

--- autocomplete_deluxe.js
+++ autocomplete_deluxe.js
@@ -21,6 +21,68 @@
     }
   };
 
+  /**
+   * Autogrow plugin which auto resizes the input of the multiple value.
+   *
+   * http://stackoverflow.com/questions/931207/is-there-a-jquery-autogrow-plugin-for-text-fields
+   *
+   */
+  $.fn.autoGrowInput = function(o) {
+
+    o = $.extend({
+      maxWidth: 1000,
+      minWidth: 0,
+      comfortZone: 70
+    }, o);
+
+    this.filter('input:text').each(function(){
+
+      var minWidth = o.minWidth || $(this).width(),
+        val = '',
+        input = $(this),
+        testSubject = $('<tester/>').css({
+          position: 'absolute',
+          top: -9999,
+          left: -9999,
+          width: 'auto',
+          fontSize: input.css('fontSize'),
+          fontFamily: input.css('fontFamily'),
+          fontWeight: input.css('fontWeight'),
+          letterSpacing: input.css('letterSpacing'),
+          whiteSpace: 'nowrap'
+        }),
+        check = function() {
+
+          if (val === (val = input.val())) {return;}
+
+          // Enter new content into testSubject
+          var escaped = val.replace(/&/g, '&amp;').replace(/\s/g,'&nbsp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+          testSubject.html(escaped);
+
+          // Calculate new width + whether to change
+          var testerWidth = testSubject.width(),
+            newWidth = (testerWidth + o.comfortZone) >= minWidth ? testerWidth + o.comfortZone : minWidth,
+            currentWidth = input.width(),
+            isValidWidthChange = (newWidth < currentWidth && newWidth >= minWidth)
+              || (newWidth > minWidth && newWidth < o.maxWidth);
+
+          // Animate width
+          if (isValidWidthChange) {
+            input.width(newWidth);
+          }
+
+        };
+
+      testSubject.insertAfter(input);
+
+      $(this).bind('keyup keydown blur update', check);
+
+    });
+
+    return this;
+  };
+
+
   Drupal.autocomplete_deluxe.empty =  {label: '- ' + Drupal.t('None') + ' -', value: "" };
 
   /**
@@ -336,6 +398,12 @@
       }
     });
 
+    jqObject.autoGrowInput({
+      comfortZone: 50,
+      minWidth: 10,
+      maxWidth: 460
+    });
+
 
     jqObject.keyup(function (event) {
       if (clear) {
@@ -346,8 +414,6 @@
         // Return false to prevent entering the last character.
         return false;
       }
-      var cur_width = jqObject.width();
-      jqObject.width(cur_width+9);
     });
   };
 })(jQuery);
