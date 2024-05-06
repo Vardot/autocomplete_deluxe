9ce9e51244cb053e7a9749e97715902cf7d750a0
Andrew Sereda <38277-sic@users.noreply.drupalcode.org>
2024-05-06 14:55:26 +0000

    Issue #3053986 by sickness29, manuga, aditya.ghan, rksyravi, Rajab Natshah, Nicolas Bouteille, borisson_, cedewey, robcarr, samar.alazzeh: Autocomplete deluxe widget doesn't work for multiple entity reference field when you add new terms and some terms are existing

--- src/Plugin/Field/FieldWidget/AutocompleteDeluxeWidget.php
+++ src/Plugin/Field/FieldWidget/AutocompleteDeluxeWidget.php
@@ -11,12 +11,12 @@ use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Site\Settings;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\Url;
 use Drupal\user\EntityOwnerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Session\AccountInterface;
 
 /**
  * Plugin implementation of the 'options_buttons' widget.
@@ -113,7 +113,6 @@ class AutocompleteDeluxeWidget extends WidgetBase implements ContainerFactoryPlu
       'delimiter' => '',
       'not_found_message_allow' => FALSE,
       'not_found_message' => "The term '@term' will be added",
-      'new_terms' => FALSE,
       'no_empty_message' => 'No terms could be found. Please type in order to add a new term.',
     ] + parent::defaultSettings();
   }
@@ -162,12 +161,6 @@ class AutocompleteDeluxeWidget extends WidgetBase implements ContainerFactoryPlu
       '#description' => $this->t('A message text which will be displayed, if the entered term was not found.'),
       '#default_value' => $this->getSetting('not_found_message'),
     ];
-    $element['new_terms'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Allow new terms'),
-      '#description' => $this->t('Should it be allowed, that user enter new terms?'),
-      '#default_value' => $this->getSetting('new_terms'),
-    ];
     $element['no_empty_message'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Empty value message.'),
@@ -190,7 +183,6 @@ class AutocompleteDeluxeWidget extends WidgetBase implements ContainerFactoryPlu
     $summary[] = $this->t('Delimiter: @delimiter', ['@delimiter' => $this->getSetting('delimiter')]);
     $summary[] = $this->t('Allow Not Found message: @not_found_message_allow', ['@not_found_message_allow' => $this->getSetting('not_found_message_allow') ? 'Yes' : 'No']);
     $summary[] = $this->t('Not Found message: @not_found_message', ['@not_found_message' => $this->getSetting('not_found_message')]);
-    $summary[] = $this->t('Allow new terms: @new_terms', ['@new_terms' => $this->getSetting('new_terms') ? 'Yes' : 'No']);
     $summary[] = $this->t('Empty value message: @no_empty_message', ['@no_empty_message' => $this->getSetting('no_empty_message')]);
 
     return $summary;
@@ -225,7 +217,7 @@ class AutocompleteDeluxeWidget extends WidgetBase implements ContainerFactoryPlu
       '#delimiter' => $settings['delimiter'] ?? '',
       '#not_found_message_allow' => $allow_message,
       '#not_found_message' => $this->t('@not_found_message', ['@not_found_message' => $not_found_message]),
-      '#new_terms' => $settings['new_terms'] ?? FALSE,
+      '#new_terms' => $this->getSelectionHandlerSetting('auto_create'),
       '#no_empty_message' => isset($settings['no_empty_message']) ? $this->t('@no_empty_message', ['@no_empty_message' => $settings['no_empty_message']]) : '',
     ];
 
@@ -233,7 +225,7 @@ class AutocompleteDeluxeWidget extends WidgetBase implements ContainerFactoryPlu
 
     // If new terms are allowed to be created, set the bundle and the uid of the
     // term.
-    if ($this->getSetting('new_terms') && $this->getSelectionHandlerSetting('auto_create') && ($bundle = $this->getAutocreateBundle())) {
+    if ($this->getSelectionHandlerSetting('auto_create') && ($bundle = $this->getAutocreateBundle())) {
       $element['#autocreate'] = [
         'bundle' => $bundle,
         'uid' => ($entity instanceof EntityOwnerInterface) ? $entity->getOwnerId() : $this->account->id(),
